f4a26f3affaf53062601c8d0f12c5280d3d7a4f6
Antoine-Goncalves-ISC-France <antoine.goncalves-isc.france@ibm.com>
2022-09-22 16:02:05 +0200

    Installation et configuration de Mongoose

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const app = express();
 const api = require('./api/v1/index');
 const cors = require('cors');
 
+const mongoose = require('mongoose');
+const connection = mongoose.connection;
+
 app.set('port', (process.env.port || 3000));
 
 app.use(cors());
 app.use('/api/v1', api);
 
-app.listen(app.get('port'), () => {
-	console.log(`Express server listening on port ${app.get('port')}`);
+mongoose.connect('mongodb://localhost:27017/whiskyapp', { useNewUrlParser: true });
+connection.on('error', (err) => {
+	console.error(`connection to MongoDB error: ${err.message}`);
+});
+
+connection.once('open', () => {
+	console.log('Connected to MongoDB');
+
+	app.listen(app.get('port'), () => {
+		console.log(`Express server listening on port ${app.get('port')}`);
+	});
 });
